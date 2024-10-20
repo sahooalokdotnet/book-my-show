ec2f6d5fbde323d2b3e57400f6bf92dbde18a3f0
aks <sahooalok.net@outlook.com>
2024-10-20 06:22:49 +0530

    first commit

--- src/main/java/com/scalar/alok/oops/bookmyshow/models/BaseModel.java
+++ src/main/java/com/scalar/alok/oops/bookmyshow/models/BaseModel.java
@@ -1,4 +1,9 @@
 package com.scalar.alok.oops.bookmyshow.models;
 
+import java.util.Date;
+
 public class BaseModel {
+    private long id;
+    private Date createdAt;
+    private Date updatedAt;
 }
