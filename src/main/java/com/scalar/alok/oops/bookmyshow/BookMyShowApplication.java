26d405df674f8082c034123b18b54cc513e1a7f9
aks <sahooalok.net@outlook.com>
2024-10-20 06:22:33 +0530

    first commit

---
+++ src/main/java/com/scalar/alok/oops/bookmyshow/BookMyShowApplication.java
@@ -0,0 +1,13 @@
+package com.scalar.alok.oops.bookmyshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMyShowApplication.class, args);
+    }
+
+}
